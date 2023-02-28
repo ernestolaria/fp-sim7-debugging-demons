cd22238d6a43f828e0494ed887d3a398e224f5c1
ernestolaria <ernestolaria@gmail.com>
2023-02-28 11:50:54 -0500

    pytest

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+python3 pytest
 python3 -m pipenv run pip freeze > requirements.txt
 python3 -m pipenv run pip install --requirement requirements.txt
 zip -r scribble-ai.zip . -x "*.git*" "*.venv*" "*.vscode*" "*.idea*" "*.DS_Store*"
